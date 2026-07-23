9dfbecbe9ee1ab4e19adc97fc0c4165fc5636617
Nathan Santos <natesantosa@gmail.com>
2026-07-23 08:24:14 -0700

    add public access block resource to resources.tf

--- bootstrap/resources.tf
+++ bootstrap/resources.tf
@@ -19,6 +19,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "terraform_locks" {
     name         = "terraform-state-locking"
     billing_mode = "PAY_PER_REQUEST"
