39416b40ad443d2430353bbc93788c012e3f4ae2
Nathan Santos <natesantosa@gmail.com>
2026-07-23 18:15:06 -0700

    add terraform config for backend and providers

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.2"
+  backend "s3" {
+    bucket         = "honeypot-devops-directive-tf-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
